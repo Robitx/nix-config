77196bbe12f392acf69d09fd7715175af06676c5
Tibor Schmidt <robitx@gmail.com>
2025-06-06 16:15:03 +0200

    refactor: extract boot configuration to module

--- configuration.nix
+++ configuration.nix
@@ -11,19 +11,9 @@ in
 {
   imports =
     [
+      ./modules/system/boot.nix
       # Include the results of the hardware scan.
     ];
-
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  boot.kernel.sysctl = {
-    "fs.file-max" = 1048576;
-    "fs.inotify.max_queued_events" = 1048576;
-    "fs.inotify.max_user_instances" = 1048576;
-    "fs.inotify.max_user_watches" = 1048576;
-  };
 
   systemd.extraConfig = ''
     DefaultLimitNOFILE=1048576
@@ -245,32 +235,6 @@ in
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "25.05"; # Did you read the comment?
-
-
-  boot.initrd.postDeviceCommands = lib.mkAfter ''
-    mkdir /btrfs_tmp
-    mount /dev/root_vg/root /btrfs_tmp
-    if [[ -e /btrfs_tmp/root ]]; then
-        mkdir -p /btrfs_tmp/old_roots
-        timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-        mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
-    fi
-
-    delete_subvolume_recursively() {
-        IFS=$'\n'
-        for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-            delete_subvolume_recursively "/btrfs_tmp/$i"
-        done
-        btrfs subvolume delete "$1"
-    }
-
-    for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
-        delete_subvolume_recursively "$i"
-    done
-
-    btrfs subvolume create /btrfs_tmp/root
-    umount /btrfs_tmp
-  '';
 
   fileSystems."/persist".neededForBoot = true;
   environment.persistence."/persist/system" = {
